a44e809bef0f16a6f6e0ccc81d320174bb2c744f
Linkon <linkon3.1416@gmail.com>
2021-09-05 12:50:42 +0600

    Add Basic Design

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import react from 'react';
-const App = () => {
-  return <>
-  Hello World
-  </>;
-};
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
 
 export default App;
