6c37134e192f0a6ff78ca6b6b5bd4cdb5347f6b8
Linkon <linkon3.1416@gmail.com>
2021-10-15 15:35:51 +0600

    Add Context API and Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,23 @@ import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/quiz' component={Quiz} />
-          <Route exact path='/result' component={Result} />
-        </Switch>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/quiz' component={Quiz} />
+            <Route exact path='/result' component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
