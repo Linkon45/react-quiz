7e7338a5146f55fa1575533e982e717bba93db04
Linkon <linkon3.1416@gmail.com>
2021-10-23 15:00:32 +0600

    Add firebase realtime database

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,24 +1,20 @@
 import classes from '../styles/Videos.module.css';
 import Video from './Video';
 import { Link } from 'react-router-dom';
+import { useVideoList } from '../hooks/useVideoList';
 export default function Videos() {
+  const { loading, error, videos } = useVideoList();
   return (
     <div className={classes.videos}>
-      <Link to='/quiz'>
-        <Video />
-      </Link>
-      <Link to='/quiz'>
-        <Video />
-      </Link>
-      <Link to='/quiz'>
-        <Video />
-      </Link>
-      <Link to='/quiz'>
-        <Video />
-      </Link>
-      <Link to='/quiz'>
-        <Video />
-      </Link>
+      {videos.length > 0 &&
+        videos.map((video) => (
+          <Link to='/quiz' key={video.youtubeID}>
+            <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+          </Link>
+        ))}
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
---
+++ src/hooks/useVideoList.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { getDatabase, ref, query, orderByKey, get } from 'firebase/database';
+
+export const useVideoList = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchVideos() {
+      const db = getDatabase();
+      const videosRef = ref(db, 'videos');
+      const videoQuery = query(videosRef, orderByKey());
+      try {
+        setError(false);
+        setLoading(true);
+
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    fetchVideos();
+  }, []);
+
+  return {
+    loading,
+    error,
+    videos,
+  };
+};
