ff2e5dfad7185e4da4739d137e907d279c6b4bba
Ali Hasan Md. Linkon <linkon3.1416@gmail.com>
2021-10-28 03:20:18 +0600

    Add fetch questions

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import Answers from '../Answers';
-import ProgressBar from '../ProgressBar';
-import MiniPlayer from '../MiniPlayer';
+import React, { useState } from "react";
+import Answers from "../Answers";
+import ProgressBar from "../ProgressBar";
+import MiniPlayer from "../MiniPlayer";
+import { useParams } from "react-router-dom";
+import { useQuestions } from "../../hooks/useQuestions";
 
 const Quiz = () => {
+  const { id } = useParams();
+  const { loading, error, questions } = useQuestions(id);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
   return (
     <>
       <h1>Pick three of your favorite Star Wars Flims</h1>
---
+++ src/hooks/useQuestions.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { get, getDatabase, ref, query, orderByKey } from "firebase/database";
+
+export const useQuestions = (videoID) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      const db = getDatabase();
+      const quizRef = ref(db, "quiz/" + videoID + "/questions");
+      const quizQuery = query(quizRef, orderByKey());
+
+      try {
+        setError(false);
+        setLoading(true);
+
+        const snapshot = await get(quizQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setQuestions((prevQuestions) => {
+            return [...prevQuestions, Object.values(snapshot.val())];
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+    fetchQuestions();
+  }, [videoID]);
+
+  return {
+    loading,
+    error,
+    questions,
+  };
+};
